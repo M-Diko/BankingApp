76167a8c600d2c4fa047d8929430aba067f5ab36
Maliviwe Diko <Maliviwe.Diko@adaptit.com>
2022-12-19 16:43:31 +0200

    third commit

--- app.js
+++ app.js
@@ -2,3 +2,31 @@ function redirect () {
     window.location = "login.html";
 }
 setTimeout('redirect()', 5000);
+
+
+function signin(clicked) {
+  alert("signing in");
+}
+
+function forgot(clicked) {
+  alert("Please visit nearest branch and reset password");
+}
+
+function prepaid() {
+    let password = prompt("Please enter your remote pin to buy prepaid mobile:", " ");
+}
+function power() {
+    let password = prompt("Please enter your remote pin to buy electricity:", " ");
+}
+function transfer() {
+    let password = prompt("Please enter your remote pin to transfer money:", " ");
+}
+function stop() {
+    let password = prompt("Please enter your remote pin to stop card:", " ");
+}
+function debit() {
+    let password = prompt("Please enter your remote pin to access debit orders:", " ");
+}
+function settings() {
+    let password = prompt("Please enter your remote pin to access app settings:", " ");
+}
